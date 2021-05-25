8e3f846ce0bc25f52d3d40d13651ecea03d8abf2
nickubed <nhpquandt@gmail.com>
2021-05-25 11:45:27 -0700

    polish it up

--- index.js
+++ index.js
@@ -3,16 +3,24 @@ const express = require('express')
 const axios = require('axios')
 const app = express()
 
+app.get('/', (req, res) => {
+    res.status(200).send('Please use /album/:artistId to search for all albums by artist or /song/:albumId to search for all songs by an album id')
+})
+
 app.get('/album/:artistId', async (req, res) => {
     // searches for all albums by artist, feed itunes artistId into params
     let response = await axios.get(`https://itunes.apple.com/lookup?id=${req.params.artistId}&entity=album`)
-    res.send(response.data)
+    res.status(200).send(response.data)
 })
 
 app.get('/song/:albumId', async (req, res) => {
     // searches for all songs by album
     let response = await axios.get(`https://itunes.apple.com/lookup?id=${req.params.albumId}&entity=song`)
-    res.send(response.data)
+    res.status(200).send(response.data)
+})
+
+app.get('*', (req, res) => {
+    res.status(404).send('404: Not Found')
 })
 
 app.listen(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`))
