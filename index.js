b7783b17fb072deb0bbe7256424969157dd6e8bd
nickubed <nhpquandt@gmail.com>
2021-05-27 11:55:38 -0700

    more descriptive messaging

--- index.js
+++ index.js
@@ -26,4 +26,4 @@ app.get('*', (req, res) => {
     res.status(404).send('404: Not Found')
 })
 
-app.listen(process.env.PORT, () => console.log(`Listening on ${process.env.PORT}`))
+app.listen(process.env.PORT || 4000, () => console.log(`Listening on ${process.env.PORT || 4000}`))
